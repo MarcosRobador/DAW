4f39efe7d7322f68f2deebb0ca40a7b3e771b691
Marcos <Marcoscabet@gmail.com>
2023-09-27 19:14:36 +0200

    Ejercicio 2

---
+++ Ej2.php
@@ -0,0 +1,19 @@
+<?php
+// Variables declaradas
+$name = "Marcos";
+$surname1 = "Robador";
+$surname2 = "Mateos";
+$email = "marcoscabet@gmail.com";
+$birthdate = 2003; 
+$numberphone = "620-19-68-52"; 
+
+// Tabla
+echo "<table border='5'>";
+echo "<tr><th>Nombre</th><td>$name</td></tr>";
+echo "<tr><th>Primer Apellido</th><td>$surname1</td></tr>";
+echo "<tr><th>Segundo Apellido</th><td>$surname2</td></tr>";
+echo "<tr><th>Email</th><td>$email</td></tr>";
+echo "<tr><th>Año de Nacimiento</th><td>$birthdate</td></tr>";
+echo "<tr><th>Móvil</th><td>$numberphone</td></tr>";
+echo "</table>";
+?>
