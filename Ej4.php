331b100787f2b5a70ca4660f1eec38b952743dd6
Marcos <Marcoscabet@gmail.com>
2023-09-28 10:48:15 +0200

    Ejeecicio 4.1

--- Ej4.php
+++ Ej4.php
@@ -1,6 +1,9 @@
 <?php
 
-$edad = 20;
+// Verifica si la edad esta en la URL
+if (isset($_GET['edad'])) {
+    // Obtiene la edad de la URL y la convierte en un entero
+    $edad = (int)$_GET['edad'];
 
 $anyoActual = date("Y");
 
@@ -16,4 +19,11 @@ echo "Edad actual: $edad años <p>";
 echo "Edad dentro de 10 años: $edadEn10  // Será el año: $anyoEn10 <p>";
 echo "Edad hace de 10 años: $edadHace10  // Será el año: $anyoHace10 <p>";
 echo "Si te jubilas a los 67 años, será el año: $anyoJubilacion ";
+
+} else {
+    // Si la edad no se ha puesto en la url
+    echo "Por favor, proporciona una edad a través de la URL, http://localhost/ActividadesPHP/PHP/Ej4.php?edad=x";
+}
+
+
 ?>
