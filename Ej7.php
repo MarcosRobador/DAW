a8799d81e1a39e4d98675927f371a69b366f39ba
Marcos <Marcoscabet@gmail.com>
2023-09-29 10:43:13 +0200

    Ejercicio 7

---
+++ Ej7.php
@@ -0,0 +1,23 @@
+<?php
+// Coeficientes de la ecuación cuadrática
+$a = 2;
+$b = 5;
+$c = 2;
+
+// Calcula el discriminante
+$discriminante = $b * $b - 4 * $a * $c;
+
+if ($discriminante > 0) {
+    // Dos soluciones reales distintas
+    $x1 = (-$b + sqrt($discriminante)) / (2 * $a);
+    $x2 = (-$b - sqrt($discriminante)) / (2 * $a);
+    echo "Dos soluciones reales distintas: x1 = $x1 y x2 = $x2";
+} elseif ($discriminante == 0) {
+    // Una solución real (solución doble)
+    $x = -$b / (2 * $a);
+    echo "Una solución real (solución doble): x = $x";
+} else {
+    // Sin soluciones reales
+    echo "No hay soluciones reales";
+}
+?>
