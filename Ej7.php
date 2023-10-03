cfd1ba3963d6bbc5fa85bff76a59cb1d147d5d06
Marcos <Marcoscabet@gmail.com>
2023-10-03 23:32:18 +0200

    ejercicio7

--- Ej7.php
+++ Ej7.php
@@ -1,25 +1,19 @@
 <?php
 
-// Coeficientes de la ecuacion 
-$a = 2;
-$b = 5;
-$c = 2;
+$a = 8;
+$b = 20;
+$c = 8;
 
-// Calcula el discriminante
-$discriminante = $b * $b - 4 * $a * $c;
+// calcula el discriminante
+// ** 2: elevado a dos
+$discriminante = $b ** 2  - 4 * $a * $c;
 
 if ($discriminante > 0) {
-    //  Soluciones distintas
-    $x1 = (-$b + sqrt($discriminante)) / (2 * $a);
-    $x2 = (-$b - sqrt($discriminante)) / (2 * $a);
+    //  soluciones distintas
+    $sol1 = (-$b + sqrt($discriminante)) / (2 * $a);
+    $sol2 = (-$b - sqrt($discriminante)) / (2 * $a);
 
-    echo "Dos soluciones distintas: x1 = $x1 y x2 = $x2";
-
-} elseif ($discriminante == 0) {
-    // Una solución real (solucion doble)
-    $x = -$b / (2 * $a);
-
-    echo "Una solución real (solución doble): x = $x";
+    echo "Dos soluciones distintas: x1 = $sol1 y x2 = $sol2";
 
 } else {
     
