74ba4739846e8d8a5bfd8c6259e587b0c9eed372
Marcos <Marcoscabet@gmail.com>
2023-10-03 22:50:44 +0200

    Ejercicio15

--- Ej15.php
+++ Ej15.php
@@ -1,14 +1,16 @@
 <?php
 
+// array vacio
 $elementos = [];
+// bucle el cual llega a 100 y genera un numero aleatorio entre 0 y 1 generando las F y las M
 for ($i = 0; $i < 100; $i++) {
     $elementos[] = (rand(0, 1) == 0) ? 'M' : 'F';
 }
 
-
-$resultado = array_reduce($elementos, function($carry, $item) {
-    $carry[$item]++;
-    return $carry;
+// array_reduce: cuenta las M Y F
+$resultado = array_reduce($elementos, function($contador, $valorAct) {
+    $contador[$valorAct]++;
+    return $contador;
 }, ['M' => 0, 'F' => 0]);
 
 
