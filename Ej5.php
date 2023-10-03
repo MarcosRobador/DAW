73b08f21579b64adc01fda93c402ea103c87b0b4
Marcos <Marcoscabet@gmail.com>
2023-10-03 12:32:00 +0200

    Ejercicio 5

--- Ej5.php
+++ Ej5.php
@@ -1,7 +1,7 @@
 <?php
 
-if (isset($_GET["dinero"])) {
-    $dinero = (int)$_GET["dinero"];
+if (isset($_GET["cantidad"])) {
+    $dinero = (int)$_GET["cantidad"];
     
     $descomposicion = array(500, 200, 100, 50, 20, 10, 5, 2, 1);
     
@@ -9,14 +9,18 @@ if (isset($_GET["dinero"])) {
     //valorActual: variable que toma el valor actual cada vez que se recorre el array
     foreach ($descomposicion as $valorActual) {
         
+        // intdiv: division de dos numeros que da como resultado un numero entero
         $cantidad = intdiv($dinero, $valorActual);
+        // se utiliza para actualizar la variable dinero de modo que si es 20 te ponga un billete de 20 y no te siga poniendo 2 de 10, 4 de 5...
         $dinero = $dinero % $valorActual;
         
-        $esBillete = $valorActual >= 5;
-        $esMoneda = !$esBillete;
+        // valorActual se compone de monedas si es menor a 5
+        $billete = $valorActual >= 5;
+        $moneda = !$esBillete;
         
-        $mensaje = $cantidad . " " . ($esBillete ? "billete" : "moneda") . ($cantidad !== 1 ? "s" : "") . " de $valorActual";
-        echo "$mensaje\n";
+        $mensaje = $cantidad . " " . ($billete ? "billete" : "moneda") . ($cantidad !== 1 ? "s" : "") . " de $valorActual";
+        echo "<p>$mensaje</p>";
+
     }
 } else {
     echo "Pon la cantidad de dinero en la URL";
