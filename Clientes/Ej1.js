f632e2aaadc422a13baa4dc4cf307a359b582c90
Marcos <Marcoscabet@gmail.com>
2023-10-04 10:51:05 +0200

    Ejercicio 1

---
+++ Clientes/Ej1.js
@@ -0,0 +1,15 @@
+function checkAge(age) {
+
+    if (age > 18) {
+  
+      return true;
+  
+    } else {
+  
+      // ...
+  
+      return confirm('¿Tus padres te permitieron?');
+  
+    }
+  
+  }
