87de115a2df023635d5a5cc9e0e1fed25c95588f
Marcos <Marcoscabet@gmail.com>
2023-10-19 10:50:55 +0200

    Ejercicio 4

--- Clientes/Actividad2/Ej4.js
+++ Clientes/Actividad2/Ej4.js
@@ -6,9 +6,18 @@ let menu = {
     title: "Mi menú"
     };
 
-   function multiplyNumeric(obj){
+    function multiplyNumeric(obj) {
+        
+        //Recorre las propiedades del objeto
+        for (let key in obj) {
+          //Si ls propiedad es un numero, lo multiplica por 2
+          if (typeof obj[key] === 'number') {
+            obj[key] *= 2;
+          }
+        }
+      }
+   //Necesario para poder realizar la funcion en el objeto menu   
+   multiplyNumeric(menu);
 
-    
-   }
-
+   alert("width = " + menu.width + " height = " + menu.height);
    
