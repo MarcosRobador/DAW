7523faa1460d338ed6a4c698b096e88d542d6603
Marcos <Marcoscabet@gmail.com>
2023-10-26 22:44:36 +0200

    Ej11

---
+++ Clientes/Actividad4/Ej11.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let str = "Peter";
+str.test = 5;
+alert(str.test);
+
+/* No funciona ni muestra nada porque no puedes agregar propiedades a 
+un valor primitivo de tipo string en JavaScript */
