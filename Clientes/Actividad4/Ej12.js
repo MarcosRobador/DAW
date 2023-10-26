fe1687943b077a66483b5324be1120ab962bbbe7
Marcos <Marcoscabet@gmail.com>
2023-10-26 23:04:54 +0200

    Ej12

---
+++ Clientes/Actividad4/Ej12.js
@@ -0,0 +1,6 @@
+"use strict"
+
+alert( 9999999999999999 );
+
+/* 9999999999999999 se redondea a 10000000000000000, que es el numero 
+más cercano que se puede representar con precision en un formato de 64 bits*/
