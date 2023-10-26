478c99cf373a0f3594b2a4fc5f77d06fc49ca8b4
Marcos <Marcoscabet@gmail.com>
2023-10-26 22:33:50 +0200

    Ej 10

--- Clientes/Actividad4/Ej10.js
+++ Clientes/Actividad4/Ej10.js
@@ -4,17 +4,18 @@ function Accumulator(startingValue) {
     this.value = startingValue;
   
     this.read = function () {
-      let userInput = parseFloat(prompt("Introduce un número:"));
+      let userInput = parseFloat(prompt("Introduce un numero:"));
       if (!isNaN(userInput)) {
         this.value += userInput;
       } else {
-        alert("Entrada no válida. Por favor, ingresa un número.");
+        alert("Entrada no valida. Por favor, ingresa un numero.");
       }
     };
   }
   
-  let accumulator = new Accumulator(1); // valor inicial 1
-  accumulator.read(); // agrega el valor introducido por el usuario
-  accumulator.read(); // agrega el valor introducido por el usuario
-  alert(accumulator.value); // muestra la suma de estos valores
+  
+  let accumulator = new Accumulator(1); // Valor inicial 1
+  accumulator.read(); // Agrega el valor introducido por el usuario
+  accumulator.read(); // Agrega el valor introducido por el usuario
+  alert(accumulator.value); // Muestra la suma de estos valores
   
