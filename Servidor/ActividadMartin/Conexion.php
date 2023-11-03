25aa168e232bd95ac663adeefa7852cdf901db75
Marcos <Marcoscabet@gmail.com>
2023-11-03 19:52:58 +0100

    act

--- Servidor/ActividadMartin/Conexion.php
+++ Servidor/ActividadMartin/Conexion.php
@@ -1,18 +1,72 @@
 <?php
-$host = 'localHost'; 
-$usuario = 'root'; 
+$host = 'localhost'; 
+$usuario = 'root';
 $contrasena = '';
-$base_datos = 'barcos'; 
+$base_datos = 'barcos';
 
 // Intenta conectar a la base de datos
 $conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
 
-// Verifica si la conexión tuvo éxito
-if ($conexion->connect_error) {
-    die("Error de conexión a la base de datos: " . $conexion->connect_error);
-}
-echo "Conexión exitosa"; // Esto se imprimirá si la conexión fue exitosa
 
+// Consulta SQL para obtener los datos de los barcos en la batalla de Guadalcanal
+$queryBarcos = "SELECT B.NOMBRE_BARCO, C.DESPLAZAMIENTO, C.NRO_CANIONES
+          FROM BARCOS B
+          JOIN BARCO_CLASE BC ON B.NOMBRE_BARCO = BC.NOMBRE_BARCO
+          JOIN CLASES C ON BC.CLASE = C.CLASE
+          JOIN RESULTADOS R ON B.NOMBRE_BARCO = R.NOMBRE_BARCO
+          JOIN BATALLAS BA ON R.NOMBRE_BATALLA = BA.NOMBRE_BATALLA
+          WHERE BA.NOMBRE_BATALLA = 'Guadalcanal'";
+
+$resultBarcos = $conexion->query($queryBarcos);
+
+// Consulta SQL para encontrar países con acorazados y cruceros
+$queryPaises = "SELECT DISTINCT C.PAIS
+          FROM CLASES C
+          INNER JOIN BARCO_CLASE BC ON C.CLASE = BC.CLASE
+          WHERE C.TIPO IN ('ACORAZADO', 'CRUCERO')
+          GROUP BY C.PAIS
+          HAVING COUNT(DISTINCT C.TIPO) = 2";
+
+$resultPaises = $conexion->query($queryPaises);
+
+// Consulta SQL para encontrar batallas con al menos tres barcos del mismo país
+$queryBatallas = "SELECT BA.NOMBRE_BATALLA, C.PAIS, COUNT(*) AS CantidadDeBarcos
+                 FROM BARCO_CLASE BC
+                 JOIN CLASES C ON BC.CLASE = C.CLASE
+                 JOIN RESULTADOS R ON BC.NOMBRE_BARCO = R.NOMBRE_BARCO
+                 JOIN BATALLAS BA ON R.NOMBRE_BATALLA = BA.NOMBRE_BATALLA
+                 GROUP BY BA.NOMBRE_BATALLA, C.PAIS
+                 HAVING CantidadDeBarcos >= 3";
+
+$resultBatallas = $conexion->query($queryBatallas);
+
+
+// Consulta SQL para encontrar los países con el mayor número de cañones
+$queryPaisesMaxCaniones = "SELECT C.PAIS, MAX(C.NRO_CANIONES) AS MaxCaniones
+          FROM CLASES C
+          GROUP BY C.PAIS";
+
+$resultPaisesMaxCaniones = $conexion->query($queryPaisesMaxCaniones);
+
+
+
+
+
+
+// Consulta SQL para encontrar las batallas en las que participaron barcos de la clase "Kongo"
+$queryBatallasConClaseKongo = "SELECT DISTINCT BA.NOMBRE_BATALLA
+          FROM BATALLAS BA
+          JOIN RESULTADOS R ON BA.NOMBRE_BATALLA = R.NOMBRE_BATALLA
+          JOIN BARCOS B ON R.NOMBRE_BARCO = B.NOMBRE_BARCO
+          JOIN BARCO_CLASE BC ON B.NOMBRE_BARCO = BC.NOMBRE_BARCO
+          WHERE BC.CLASE = 'KONGO'";
+
+$resultBatallasConClaseKongo = $conexion->query($queryBatallasConClaseKongo);
+
+
+
+
+
+// Cerrar la conexión a la base de datos .
 $conexion->close();
 ?>
-
--- Servidor/ActividadMartin/Vista.php
+++ Servidor/ActividadMartin/Vista.php
@@ -1,3 +1,6 @@
+<?php
+include 'Conexion.php'; // Incluye el archivo de conexión
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Mi Sitio Web</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Avenir">
 </head>
 <body>
     <header>
@@ -13,15 +17,109 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Acerca de</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Guadalcanal</a></li>
+                <li><a href="#">Acorazados y cruceros</a></li>
+                <li><a href="#">Batallas con 3 barcos</a></li>
+                <li><a href="#">Max cañones</a></li>
+                <li><a href="#">Kongo</a></li>
+
             </ul>
         </nav>
     </header>
-    <footer>
-        <p>&copy; 2023 Mi Sitio Web - Todos los derechos reservados.</p>
-    </footer>
+
+
+<?php
+if ($resultBarcos->num_rows > 0) {
+    // Comprueba si hay resultados de la consulta (barcos en la Batalla de Guadalcanal)
+    echo "<h2>Barcos en la Batalla de Guadalcanal</h2>"; 
+    echo "<table border='5'>"; 
+    echo "<tr><th>Nombre del Barco</th><th>Desplazamiento</th><th>Cantidad de Cañones</th></tr>"; 
+    while ($row = $resultBarcos->fetch_assoc()) {
+        // Itera a través de los resultados y muestra cada barco en una fila de la tabla
+        echo "<tr>"; 
+        echo "<td>" . $row["NOMBRE_BARCO"] . "</td>"; 
+        echo "<td>" . $row["DESPLAZAMIENTO"] . "</td>"; 
+        echo "<td>" . $row["NRO_CANIONES"] . "</td>"; 
+        echo "</tr>";
+    }
+    echo "</table>"; 
+} else {
+    // Si no hay resultados.
+    echo "<h2>No se encontraron barcos en la batalla de Guadalcanal.</h2>";
+}
+
+
+
+if ($resultPaises->num_rows > 0) {
+    echo "<h2>Países con Acorazados y Cruceros</h2>";
+    echo "<table border='5'>";
+    echo "<tr><th>País</th></tr>";
+    while ($row = $resultPaises->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["PAIS"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h2>No se encontraron países con acorazados y cruceros.</h2>";
+}
+
+
+if ($resultBatallas->num_rows > 0) {
+    echo "<h2>Batallas con al menos tres barcos del mismo país:</h2>";
+    echo "<table border='5'>";
+    echo "<tr><th>Nombre de la Batalla</th><th>País</th><th>Cantidad de Barcos</th></tr>";
+    while ($row = $resultBatallas->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["NOMBRE_BATALLA"] . "</td>";
+        echo "<td>" . $row["PAIS"] . "</td>";
+        echo "<td>" . $row["CantidadDeBarcos"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h2>No se encontraron batallas con al menos tres barcos del mismo país.</h2>";
+}
+
+
+
+
+if ($resultPaisesMaxCaniones->num_rows > 0) {
+    echo "<h2>Países con el Mayor Número de Cañones</h2>";
+    echo "<table border='5'>";
+    echo "<tr><th>País</th><th>Número Máximo de Cañones</th></tr>";
+    while ($row = $resultPaisesMaxCaniones->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["PAIS"] . "</td>";
+        echo "<td>" . $row["MaxCaniones"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h2>No se encontraron países con el mayor número de cañones.</h2>";
+}
+
+
+
+
+if ($resultBatallasConClaseKongo->num_rows > 0) {
+    echo "<h2>Batallas con Barcos de la Clase 'Kongo'</h2>";
+    echo "<table border='5'>"; // Usamos "table" en lugar de "ul" para una tabla
+    echo "<tr><th>Nombre de la Batalla</th></tr>"; // Encabezado de la tabla
+    while ($row = $resultBatallasConClaseKongo->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["NOMBRE_BATALLA"] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h2>No se encontraron batallas con barcos de la clase 'Kongo'.</h2>";
+}
+
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
+
 </body>
 </html>
