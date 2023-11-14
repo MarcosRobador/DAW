99edaa595a24de33cd8c4077ddb6d3e02378f2d0
Marcos <Marcoscabet@gmail.com>
2023-11-14 13:22:40 +0100

    act modo oscuro

--- Libre_Conf/Portfolio/script.js
+++ Libre_Conf/Portfolio/script.js
@@ -1,6 +1,11 @@
 document.getElementById('toggleTheme').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
-    this.textContent = document.body.classList.contains('dark-mode') 
-                        ? 'Cambiar a Modo Claro' 
-                        : 'Cambiar a Modo Oscuro';
-  });
+    let themeIcon = document.getElementById('themeIcon');
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.src = 'img/sol.png';
+        themeIcon.alt = 'Modo Claro';
+    } else {
+        themeIcon.src = 'img/luna.png';
+        themeIcon.alt = 'Modo Oscuro';
+    }
+});
