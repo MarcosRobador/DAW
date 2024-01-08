0d07a9c7df18e4cb7e1fac678470443f98638a57
Marcos <Marcoscabet@gmail.com>
2024-01-08 14:04:36 +0100

    act

---
+++ Libre_Conf/Cuesta de enero HLC/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-git').addEventListener('click', function() {
+    document.getElementById('git-section').classList.toggle('hidden');
+});
+
+document.getElementById('btn-docker').addEventListener('click', function() {
+    document.getElementById('docker-section').classList.toggle('hidden');
+});
