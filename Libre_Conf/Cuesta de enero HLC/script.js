9bda3cc00097d6ed0ad9169b144a1eca27cb96a5
Marcos <Marcoscabet@gmail.com>
2024-01-09 09:16:06 +0100

    fin

--- Libre_Conf/Cuesta de enero HLC/script.js
+++ Libre_Conf/Cuesta de enero HLC/script.js
@@ -5,3 +5,12 @@ document.getElementById('btn-git').addEventListener('click', function() {
 document.getElementById('btn-docker').addEventListener('click', function() {
     document.getElementById('docker-section').classList.toggle('hidden');
 });
+
+document.getElementById('btn-git').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'rgb(255, 81, 0)';
+});
+
+document.getElementById('btn-docker').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#0da9fd';
+});
+
