26a8b8d8d23638705d6bdbb2dfd87d59ece08273
Marcos <Marcoscabet@gmail.com>
2024-01-09 13:01:56 +0100

    002

--- Cliente/Actividad4.1:Dom/Folder001.js
+++ Cliente/Actividad4.1:Dom/Folder001.js
@@ -7,6 +7,20 @@ Itera sobre cada uno de estos nodos y los muestra en la consola. */
 var contenedor = document.getElementById('contenedor');
 var nodosHijos = contenedor.childNodes;
 nodosHijos.forEach(function(nodo) {
-    console.log(nodo);
+console.log(nodo);
 });
 
+// 002
+
+/* El código selecciona y muestra el primer hijo del div con id contenedor:
+Selecciona el div usando getElementById.
+Obtiene el primer hijo del div con firstChild.
+Muestra este nodo en la consola. */
+
+var contenedor = document.getElementById('contenedor');
+var primerHijo = contenedor.firstChild;
+console.log(primerHijo);
+
+// 003
+
+/*  */
