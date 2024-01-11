6ddc124fc6007ee73c0f2f2a2cdbe5ddcfc4c27f
Marcos <Marcoscabet@gmail.com>
2024-01-12 00:29:50 +0100

    004hideTextButton

---
+++ Cliente/Actividad4.2:DOM/004hideTextButton/004hideTextButton.js
@@ -0,0 +1,8 @@
+"use strict"
+
+/*La funcin oculta el elemento con el id 'texto' 
+al establecer su estilo 'display' en 'none'*/
+
+function desaparece() {
+    document.getElementById('texto').style.display = 'none';
+}
