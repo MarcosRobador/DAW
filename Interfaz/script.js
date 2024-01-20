60ba4bd6bbae83092a642888665914d9a74f5ac7
Marcos <Marcoscabet@gmail.com>
2024-01-20 20:00:55 +0100

    img que cambia segun pasas el raton

---
+++ Interfaz/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+function cambiarImagen(modo, imagenOficina) {
+    let imagen = document.getElementById(imagenOficina);
+    if(modo === 'noche') {
+        imagen.src = 'img/oficina-noche.png';
+    } else {
+        imagen.src = 'img/oficina-dia.png';
+    }
+}
+
+function cambiarImagen2(modo, imagenFabrica) {
+    let imagen = document.getElementById(imagenFabrica);
+    if(modo === 'noche') {
+        imagen.src = 'img/fabrica-noche.png';
+    } else {
+        imagen.src = 'img/fabrica-dia.png';
+    }
+}
+
+function cambiarImagen3(modo, imagenAlmacen) {
+    let imagen = document.getElementById(imagenAlmacen);
+    if(modo === 'noche') {
+        imagen.src = 'img/almacen-noche.png';
+    } else {
+        imagen.src = 'img/almacen-dia.png';
+    }
+}
+
+
